450fdcab5aea42209016cfff3e4b8628c888fd3a
Lukas Dahlgren <Lucasdahlgren@gmail.com>
2018-11-09 16:44:28 +0100

    showsummary now persists through site change.

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -16,8 +16,12 @@ export default class Menu extends Component{
         this.state = {
             dest : "",
             items: [],
+            serieslength:0,
+            loadedlength:0,
             series:[],
+            done:false,
             client: new RequestService(),
+            serieloaded:false,
             site:"Serie Overview" // default TODO - cleaner
         };
         this.src = React.createRef();
@@ -50,20 +54,38 @@ export default class Menu extends Component{
                 let parsedArray = extractAllmkv(v, this.state.dest);
                 let testitems = parsedArray.filter(val => val!==undefined);
                 let series =  getSeries(testitems, this.state.client);
+                this.setState({
+                    serieslength:series.length
+                })
                 for(let seriePromise of series){
                     seriePromise.then(serie =>{
                         if(serie.key !== undefined){
                             this.setState({
+                                loadedlength:this.state.loadedlength+1,
                                 series:[...this.state.series, serie].sort((a,b) => {
                                     const A = a.props.name
                                     const B = b.props.name
                                     return A > B ? 1 : B > A ? -1 : 0;
                                 })
                             },() => console.log(this.state))
+                        }else{
+                            this.setState({
+                                loadedlength:this.state.loadedlength+1
+                            })
                         }
                     })
                 }
             })
+        }
+    }
+    componentDidUpdate(prevProps, prevState){
+        if(this.state){
+            if(this.state.loadedlength === this.state.serieslength && !this.state.done){
+                this.setState({
+                    serieloaded:true,
+                    done:true
+                })
+            }
         }
     }
     render(){
@@ -88,8 +110,8 @@ export default class Menu extends Component{
         return(
             <React.Fragment>
                 <Default />
-                {/* implement store so that we wont have to refetch and mess everything up. */}
-                {this.state.site === 'Calendar'? <Calendar />: <SerieSummary client={this.state.client} series={this.state.series}/>}
+                {/* implement store so that we wont have to refetch. */}
+                {this.state.site === 'Calendar'? <Calendar />: <SerieSummary preloaded={this.state.serieloaded} client={this.state.client} series={this.state.series}/>}
                 {/* <SerieSummary client={this.state.client} series={this.state.series}/> */}
             </React.Fragment>
         )
--- src/components/SerieSummary.js
+++ src/components/SerieSummary.js
@@ -10,6 +10,7 @@ export default class SerieSummary extends Component{
         super(props)
         this.state = {
             series: this.props.series,
+            preloaded:this.props.preloaded,
             activated:false,
             showing:false,
             sorted:true,
@@ -33,7 +34,7 @@ export default class SerieSummary extends Component{
                 <div className="src-struct">
                     <h2 className="heading-src">Series structure</h2>
                     <div className="summary-struct">
-                        {this.state.activated ? this.props.series.map(value => value): (!this.state.sorted && this.state.series.length > 0) ? <div> Loading series....</div> : null}
+                        {(this.state.activated || this.state.preloaded) ? this.props.series.map(value => value): (!this.state.sorted && this.state.series.length > 0) ? <div> Loading series....</div> : null}
                     </div>
                 </div>
             </React.Fragment>
