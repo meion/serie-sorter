b57bda71a83e5ccacaf0a1cf7cc58b4d8ea5833c
Lukas Dahlgren <Lucasdahlgren@gmail.com>
2018-11-11 17:37:15 +0100

    Seriesummary uses store.

--- src/components/SerieSummary.js
+++ src/components/SerieSummary.js
@@ -1,38 +1,25 @@
 import React, { Component } from 'react';
 import './style.css';
+import {actions} from '../store';
+import {connect} from 'unistore/react';
 
 export default class SerieSummary extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            series: this.props.series,
-            preloaded:this.props.preloaded,
-            activated:false,
-            showing:false,
-            sorted:true,
-            mounted:false,
-            client: this.props.client
-        }
-    }
-    componentDidUpdate(prevProps, prevState){
-        if(prevProps.series !== this.props.series){
-            if(!this.state.activated){
-                this.setState({
-                    activated:true
-                }, () => this.forceUpdate())
-            }
-        }
-    }
     render(){
+        let Summary = connect('series', actions)(
+            ({series}) => {
+                return(
+                    <React.Fragment>
+                        <div className="src-struct">
+                            <h2 className="heading-src">Series structure</h2>
+                            <div className="summary-struct">
+                                {series}
+                            </div>
+                        </div>
+                    </React.Fragment>
+                )
+        })
         return(
-            <React.Fragment>
-                <div className="src-struct">
-                    <h2 className="heading-src">Series structure</h2>
-                    <div className="summary-struct">
-                        {(this.state.activated || this.state.preloaded) ? this.props.series.map(value => value): (!this.state.sorted && this.state.series.length > 0) ? <div> Loading series....</div> : null}
-                    </div>
-                </div>
-            </React.Fragment>
+            <Summary />
         )
     }
 }
